8984058af213ef2fa77ba81eae88aa8484ec88ed
Arya <aryaks726@gmail.com>
2022-08-07 13:59:43 +0545

    test

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features",plugin ="json:target/jsonReports/cucumber-report.json", glue = {"stepDefinations"})
+@CucumberOptions(features = "src/test/java/features",plugin ="json:target/jsonReports/cucumber-report.json", glue = {"stepDefinations"}, tags= "@AddPlace")
 public class TestRunner {
 //tags= "@AddPlace"  put it in cucumberOptions above after glue
 }
